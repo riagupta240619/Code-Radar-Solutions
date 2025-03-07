b5745eb6ee50fedc965c377c6deb45d1889c13cc
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-03-07 18:29:02 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -7,7 +7,7 @@ int main(){
         printf("Digit");
     }
     else if (isalpha(c)){
-        if (c == "a" ||c == "e" ||c == "i" || c == "o" ||c == "u" || c == "A" ||c = "E" ||c == "I" || c == "O" ||c == "U" ){
+        if (c == 'a' ||c == 'e' ||c == 'i' || c == 'o' ||c == 'u' || c == 'A' ||c = 'E' ||c == 'I' || c == 'O' ||c == 'U' ){
             printf("Vowel");
         }
         else{
