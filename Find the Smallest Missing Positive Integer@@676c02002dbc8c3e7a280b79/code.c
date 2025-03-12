5091a72caaaf3651f833013b338bba03cb0470cd
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-03-12 12:09:36 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -46,7 +46,7 @@ int main(){
             }
             // printf("%d %d \n",e,i);
             if ((e == 0) & (i>0)) {
-                printf(" %d",e);
+                printf(" %d",i);
             }
         }
     }
