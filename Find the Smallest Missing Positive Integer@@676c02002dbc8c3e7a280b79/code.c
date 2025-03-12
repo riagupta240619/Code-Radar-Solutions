dc0708aab7abb43153bc93e739841501181ab4c3
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-03-12 11:58:45 +0530

    Solution to Find the Smallest Missing Positive Integer

---
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -0,0 +1,52 @@
+// Your code here...
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+int max(int arr[], int a){
+    int max = arr[0];
+    for(int i =0; i<a; i++){
+        if (arr[i] > max) max = arr[i];
+    }
+    return max;
+}
+
+int min(int arr[], int a){
+    int min = arr[0];
+    for(int i =0; i<a; i++){
+        if (arr[i] < min) min = arr[i];
+    }
+    return min;
+}
+
+int main(){
+    int a;
+    scanf("%d", &a);
+    int *myarray = malloc(a * sizeof(int));
+    for(int i =0; i<a; i++){
+        scanf("%d", &myarray[i]);
+    }
+    int e = 0;
+    for(int i = 0;i<a;i++) {
+        if(myarray[i] == 1) {
+            e++;
+        }
+    }
+    if(e == 0) {
+        printf("1");
+    } else {
+        int maxx = max(myarray,a);
+        int minn = min(myarray,a);
+        for(int j = 0;j<a;j++) {
+            int e = 0;
+            for(int i = minn;i<=maxx;i++) {
+                if(myarray[j] == i) {
+                    e++ ;
+                }
+            }
+            if(e == 0) {
+                printf("%d",j);
+            }
+        }
+    }
+}
