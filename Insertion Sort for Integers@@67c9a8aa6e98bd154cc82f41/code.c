bfe042f73e3533fa9fa94d32eb68d3db159cd2a7
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-03-28 19:42:39 +0530

    Solution to Insertion Sort for Integers

---
+++ Insertion Sort for Integers@@67c9a8aa6e98bd154cc82f41/code.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void insertionSort(int arr[], int n){
+    for(int i = 0; i< n; i++){
+        for(int j = 0; j<n; j++){
+            if(arr[i]<arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+void printArray(int arr[], int n){
+    for(int i = 0;i<n; i++){
+        printf("%d", arr[i]);
+    }
+}
