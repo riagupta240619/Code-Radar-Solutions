0f7e00ebc0620780e957a390f03089f6595c9cf4
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-03-29 22:42:25 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+void caesarCipher(char msg[100], int shift, char enc[100]){
+    for(int i = 0;i<n;i++){
+        msg[i] = msg[i + shift];
+    }
+    for(int i = 0; i<n; i++){
+        msg[i] = enc[i];
+    }
+}
