a0ae4e83adcd7b317da1d84fb81e84dc87aff6ad
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-03-30 14:37:18 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -2,29 +2,24 @@
 
 void frequency(int arr[], int n, int arr1[]) {
     int visited[n];
-
-    // Initialize the visited array with 0
     for (int i = 0; i < n; i++) {
         visited[i] = 0;
-        arr1[i] = 0; // Initialize arr1 to store frequency counts
+        arr1[i] = 0;
     }
-
     for (int i = 0; i < n; i++) {
         if (visited[i] == 1) {
             continue;
         }
-
         int count = 1;
         for (int j = i + 1; j < n; j++) {
             if (arr[i] == arr[j]) {
                 count++;
-                visited[j] = 1; // Mark as visited
+                visited[j] = 1;
             }
         }
         arr1[i] = count;
     }
 }
-
 void bubble_Sort(int arr1[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -36,21 +31,15 @@ void bubble_Sort(int arr1[], int n) {
         }
     }
 }
-
 int main() {
     int n;
     scanf("%d", &n);
-
-    int arr[n], arr1[n]; // Corrected declaration of arrays
-
+    int arr[n], arr1[n];
     for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]); // Correct input loop
+        scanf("%d", &arr[i]);
     }
-
     frequency(arr, n, arr1);
     bubble_Sort(arr1, n);
-
-    printf("%d\n", arr1[n - 1]); // Print the highest frequency
-
+    printf("%d\n", arr1[n - 1]);
     return 0;
 }
