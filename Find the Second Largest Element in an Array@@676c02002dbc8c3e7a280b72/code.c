293429fd2fee25b80414b079242b1eb586180ce0
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-03-30 12:01:18 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -22,7 +22,8 @@ void bubble_sort(int arr[], int n){
         }
     }
     if(arr[n-2] == arr[n]){
-        return -1;
+        printf("-1");
+        return;
     }
     printf("%d", arr[n-2]);
     
