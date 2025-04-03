28ebda6789d7e8a43d8b13f361d64d4e8e921b69
riagupta240619 <ria0619.becse24@chitkara.edu.in>
2025-04-03 22:14:35 +0530

    Solution to Find the Majority Element in an Array

--- Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
+++ Find the Majority Element in an Array@@676c02002dbc8c3e7a280b76/code.c
@@ -4,7 +4,6 @@ void frequency(int arr[], int n, int arr1[]) {
     int visited[n];
     for (int i = 0; i < n; i++) {
         visited[i] = 0;
-        arr1[i] = 0;
     }
     for (int i = 0; i < n; i++) {
         if (visited[i] == 1) {
@@ -21,12 +20,12 @@ void frequency(int arr[], int n, int arr1[]) {
     }
 }
 void bubble_Sort(int arr1[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (arr1[j] > arr1[j + 1]) {
-                int temp = arr1[j];
-                arr1[j] = arr1[j + 1];
-                arr1[j + 1] = temp;
+    for (int i = 0; i < n ; i++) {
+        for (int j = i+1; j < n ; j++) {
+            if (arr1[i] > arr1[j]) {
+                int temp = arr1[i];
+                arr1[i] = arr1[j];
+                arr1[j] = temp;
             }
         }
     }
